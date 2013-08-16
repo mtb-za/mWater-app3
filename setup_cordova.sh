6c7292ed893279b65d1c27cdfde373961b9ce8c7
Clayton Grassick <clayton@claytronics.org>
2013-08-15 23:01:34 -0400

    Added ACRA to cordova

--- setup_cordova.sh
+++ setup_cordova.sh
@@ -11,6 +11,7 @@ cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device
 cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-network-information.git
 cordova plugin add https://github.com/mWater/cordova-plugin-camera-foreground.git
 cordova plugin add https://github.com/mWater/OpenCVActivityPlugin.git
+cordova plugin add https://github.com/mWater/cordova-plugin-acra.git
 rm platforms/android/res/drawable-hdpi/icon.png
 rm platforms/android/res/drawable-ldpi/icon.png
 rm platforms/android/res/drawable-mdpi/icon.png
@@ -24,4 +25,5 @@ echo "key.alias=mwater" >> ./platforms/android/ant.properties
 echo "TODO: Now update config.xml in android to run index_cordova.html?cordova= and other values not copied" 
 echo "TODO: Adjust minSdkVersion in AndroidManifest.xml to 8"
 echo "TODO: Set versionCode and versionName in AndroidManifest.xml for EACH release" 
+echo 'TODO: Add android:name="co.mwater.acraplugin.MyApplication" to application element of AndroidManifest.xml'
 cd ..
