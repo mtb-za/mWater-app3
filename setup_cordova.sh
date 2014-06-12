a121bf8b30a598f4f5253c88be96459ece83448a
Clayton Grassick <clayton@claytronics.org>
2014-06-12 13:51:50 -0400

    Doc fixes

--- setup_cordova.sh
+++ setup_cordova.sh
@@ -26,8 +26,6 @@ rm -r www/*
 
 echo "key.store=/home/clayton/.ssh/mwater.keystore" >> ./platforms/android/ant.properties
 echo "key.alias=mwater" >> ./platforms/android/ant.properties
-# echo "TODO: Adjust minSdkVersion in AndroidManifest.xml to 8"
-# echo "TODO: Set versionCode and versionName in AndroidManifest.xml for EACH release" 
 echo 'TODO: Add android:name="co.mwater.acraplugin.MyApplication" to application element of AndroidManifest.xml'
 echo 'TODO: Add <uses-feature android:name="android.hardware.camera" android:required="false" /> to AndroidManifest.xml'
 echo 'TODO: Add <uses-feature android:name="android.hardware.bluetooth" android:required="false" /> to AndroidManifest.xml'
