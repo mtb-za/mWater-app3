94faaa15cb935f520d818e45d3db9a94d583681b
Clayton Grassick <clayton@claytronics.org>
2014-07-08 15:13:18 -0400

    Doc cleanup

--- build_cordova_release.sh
+++ build_cordova_release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "You *MUST* update version in app/cordova/config.xml first to currently deployed version."
-#echo "You *MUST* update versionName and versionCode in cordova/platforms/android/config.xml first to currently deployed version."
 echo "Only run this *immediately* after a grunt deploy"
 read
 cd cordova
